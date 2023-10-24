05b1a4b932c75665a16171aa55e31464a707cfb0
sidmeslem <49337176+sidmeslem@users.noreply.github.com>
2023-10-24 10:54:28 +0200

    Update salesEmployee.js

--- salesEmployee.js
+++ salesEmployee.js
@@ -4,29 +4,54 @@ const router = express.Router()
 const app = express()
 const PORT = process.env.PORT || 8087
 
+const salesEmployeeList = [
+    {hrMatricule: "46379313", firstName: "Manon", lastName: "Madeleine"},
+    {hrMatricule: "86244356", firstName: "Maxime", lastName: "Marin"},
+    {hrMatricule: "18257025", firstName: "Alex", lastName: "Rolland"},
+    {hrMatricule: "66537895", firstName: "Chloé", lastName: "Odile"},
+    {hrMatricule: "40093261", firstName: "Frank", lastName: "Marcel"},
+]
+
 app.use(express.json())
 
 const createPost = (req, res, next) => {
-    console.log('MESSAGE RECEIVED FROM EBX\n', req.body)
+    console.log('MESSAGE RECEIVED FROM EBX\n')
     next()
 }
 
-router.post('/salesEmployee', createPost, (req, res) => {
+router.get('/getList', function(req, res) {
+    res.json(salesEmployeeList)
+})
+
+
+router.get('/salesEmployee', createPost, function(req, res){
     console.log("HR Matricule received : " + req.query.hrMatricule);
-    res.json({
-        "hrMatricule": req.query.hrMatricule,
-        "firstName": "Hello",
-        "lastName": "World",
+
+    var found = false;
+    for(let i = 0; i < salesEmployeeList.length; i++) {
+        let obj = salesEmployeeList[i];
+        if (obj["hrMatricule"] == req.query.hrMatricule) {
+            found = true;
+            res.json({
+                "hrMatricule": req.query.hrMatricule,
+                "firstName": "Hello",
+                "lastName": "World",
+            })
+        }
     }
-    )
+
+    if (!found) {
+        res.json({
+            "hrMatricule": req.query.hrMatricule,
+            "firstName": "Not found",
+            "lastName": "Not found",
+        })
+    } 
 })
 
 
 app.use('/', router)
 
 app.listen(PORT, () => {
-    console.log(
-        `server ready \n\n[SAP HR]\n`
-    )
+    console.log(`server ready \n\n[SAP HR]\n`)
 })
-
